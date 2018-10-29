6964c1412e716455050c5a8269d35cd0d55d2bfa
Nightmayr <umayr23@gmail.com>
2018-10-29 18:25:45 +0000

    Set up index page with bootstrap styling and navbar. main.css created to add further styling. shop.ejs created but shop route not configured in app.js

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ var express = require('express'),
     mongoose = require('mongoose');
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 
+// Index route
 app.get("/", function (req, res) {
     res.render("index");
 });
 
 
-app.listen(3000, function (req, res) {
-    console.log("Server listening on port 3000");
+
+
+app.listen(3001, function (req, res) {
+    console.log("Server listening on port 3001");
 });
